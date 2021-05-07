cfe2f5b2a983481b5e23128e986d7e00cdd262bb
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-05-06 19:08:30 -0500

    bot v1.1

--- Python/whatsapp_bot.py
+++ Python/whatsapp_bot.py
@@ -1,14 +1,42 @@
 import pyautogui, webbrowser, time
 
 def run():
-    target_number="+573002484003"
-    msg = ["holo","prro","te escribo","dese un bot que programe xd"]
+
+    print(
+    """
+            ________
+           /_______/
+           \ \    / /	    _           _
+         ___\ \__/_/___    | |__   ___ | |_
+        /____\ \______/\   | '_ \ / _ \| __|
+        \ \   \/ /   / /   | |_) | (_) | |
+         \ \  / /\  / /    |____/ \___/\__|
+          \ \/ /\ \/ /
+           \_\/  \_\/
+
+    """)
+
+    # request numbers for send sms
+    target_number=input(
+    """
+        Ingrese el numero al que desea escribir:
+        (Ej : +573000000)
+        
+        -> """)
+
+    msg = input("\n\r"+" "*8+"Ingrese el mensaje que desea enviar:\n\r"+" "*8+"-> ")
+    target_number.replace(" ", "")
+    frecuency = int(input("\n\r"+" "*8 +"Cuantas veces desea enviar el mensaje:\n\r"+" "*8+"-> "))
+
+    # connect to whatsapp web
     webbrowser.open("https://web.whatsapp.com/send?phone="+target_number)
     time.sleep(5)
-    for i in range(100):
-        for i in range(4):
-            pyautogui.write(msg[i])
-            pyautogui.press("enter")
+
+    # sending message
+    for i in range(frecuency):
+        pyautogui.write(msg)
+        pyautogui.press("enter")
+    print("\n\r"+" "*8+"que se siente el spam perro?[FINISHED PROGRAM]")
 
 if __name__=='__main__':
     run()
